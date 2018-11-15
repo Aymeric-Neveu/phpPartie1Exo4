c6fab124a052c9ef64a77f130a95ec1229805e5c
Aymeric Neveu <emploi.neveu@gmail.com>
2018-11-15 10:46:03 +0100

    exo4

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>PHP</title>
+  </head>
+  <body>
+    <?php
+    $string ="allez les bleus";
+    $int="12";
+    $float="25,10";
+    $boolean="true";
+    echo "$string $int $float $boolean";
+
+    ?>
+  </body>
